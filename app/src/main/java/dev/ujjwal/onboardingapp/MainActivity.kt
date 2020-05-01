9b852ce4970006d4488c2d092f7a42276a58e463
ujjwal <ujjwalmaity98@gmail.com>
2020-05-01 15:42:44 +0530

    Making notification bar transparent

--- app/src/main/java/dev/ujjwal/onboardingapp/MainActivity.kt
+++ app/src/main/java/dev/ujjwal/onboardingapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package dev.ujjwal.onboardingapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.text.Html
 import android.view.View
+import android.view.WindowManager
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +22,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Making notification bar transparent
+        changeStatusBarColor()
 
         val sliderAdapter = SliderAdapter(this)
         view_pager.adapter = sliderAdapter
@@ -63,6 +70,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Making notification bar transparent
+     */
+    private fun changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
 
     private fun addDotIndicator(currentPage: Int) {
         val dots = arrayListOf<TextView>()
